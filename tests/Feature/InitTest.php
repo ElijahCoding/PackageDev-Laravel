8f261661fe6f41503a169f6c9c0c4c0a8048a0b8
ElijahCoding <cuiyeqing960904@gmail.com>
2019-12-18 22:19:13 +0300

    tests set up

---
+++ tests/Feature/InitTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Package\Press\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class InitTest extends TestCase
+{
+    /** @test */
+    public function it_can_find_the_test_markdown_files()
+    {
+        $this->assertTrue(True);
+    }
+}
